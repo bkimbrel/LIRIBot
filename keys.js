8706e3a9809ccb77c963bc99c33752b55c7bdfce
bkimbrel <bkimbrel2@gmail.com>
2017-07-23 19:17:48 -0600

    working copy

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'pqpYssWVFHEjZy5cpZatLXb9F',
+  consumer_secret: 'bVlgycf9nxXkRGtF8JwN0TPucTTdOyMfVL2bHJTG19JRpwlXh5',
+  access_token_key: '888597397189689345-LeNX1XjVfDxI9b0xubdV4DBuBDXOrKX',
+  access_token_secret: '	hNYZJBhrxMsOCn6I6ElshV9LmxY9GAQm36Qz0t3EMuc8r',
+}
